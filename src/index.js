fa9000d546394a8a99f71e866c0536be6dce3211
Chromium <chromiumiscool@outlook.com>
2021-01-01 20:20:59 +0100

    Styling fixes + timestamps

--- src/index.js
+++ src/index.js
@@ -95,6 +95,11 @@ app.get('/ticket/:ticketID', checkAuth, async function (req, res) {
   });
   r.table('chatlogs').get(req.params.ticketID).run(async (err, callback) => {
     chatlog = callback;
+
+    let chatlogId = BigInt.asUintN(64, chatlog.id);
+    let dateBits = Number(chatlogId >> 22n);
+    let timestamp = new Date(dateBits + 1420070400000);
+
     for (var i = chatlog.logs.length - 1; i >= 0; i--) {
       let userID = chatlog.logs[i].userID
       if(typeof userID == "undefined") continue;
@@ -119,7 +124,8 @@ app.get('/ticket/:ticketID', checkAuth, async function (req, res) {
       title: `Ticket ${ticketName}`,
         ticket,
         chatlog,
-        avatars
+        avatars,
+        timestamp
     });
   });
 });
